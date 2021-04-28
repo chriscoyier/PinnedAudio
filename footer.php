d8d238bf0baa101f0a5873bd3e988aaf2b92d2e0
Chris Coyier <chriscoyier@gmail.com>
2021-04-28 13:07:51 -0700

    break cache

--- footer.php
+++ footer.php
@@ -16,9 +16,9 @@
 
   <?php wp_footer(); ?>
   
-  <script type="module" src="<?php bloginfo('stylesheet_directory'); ?>/javascripts/podcast-player.js?v1.0"></script>
-  <script type="module" src="<?php bloginfo('stylesheet_directory'); ?>/javascripts/turbo.js?v1.0"></script>
-  <script src="<?php bloginfo('stylesheet_directory'); ?>/javascripts/main.js?v1.0"></script>
+  <script type="module" src="<?php bloginfo('stylesheet_directory'); ?>/javascripts/podcast-player.js?v1.1"></script>
+  <script type="module" src="<?php bloginfo('stylesheet_directory'); ?>/javascripts/turbo.js?v1.1"></script>
+  <script src="<?php bloginfo('stylesheet_directory'); ?>/javascripts/main.js?v1.1"></script>
 
 </body>
 
